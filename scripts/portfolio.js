523ade8ed07ff6558021ff630162e4a745fdf789
Ganon4Prez <88854718+ZachMoss31@users.noreply.github.com>
2025-01-18 17:14:44 -0700

    Moving the portfolio list into a dictionary so i can interact with a modal

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,6 +1,7 @@
 // Class object for storing Portfolio elements (should create a pop-out element with 'x' exit button?)
 class PortfolioObject {
     constructor(){
+        this.id = ''; // The HTML id to add
         this.display = ''; //This is the main gif or image on the card, as well as on top of the pop up
         this.title = '';
         this.overview = '';
@@ -11,10 +12,14 @@ class PortfolioObject {
     }
 }
 
-portfolioObjectList = [];
-function FillPortfolioSection(){
+// ----------------------------------------------------------------------------------------------------- TO DO::
+// This will be updated with a more data-driven approach later with .txt files to pull from 'for each directory in features', etc
+portfolioObjectList = {};
+function FillPortfolioObjects(){
+    
     // Bits of Shadow
     bits = new PortfolioObject();
+    bits.id = 'bits';
     bits.display = '../data/features/bits-of-shadow/bits.png';
     bits.title = 'Bits of Shadow';
     bits.overview = 'Unity 3D project exploring 3D game engines and how they are used';
@@ -22,10 +27,12 @@ function FillPortfolioSection(){
     bits.tagSet.push('unity');
     bits.featureSet.push('../data/features/bits-of-shadow/Switch.gif','../data/features/bits-of-shadow/TestScene.gif');
     bits.other.push('Test', 'Object','Data');
-    portfolioObjectList.push(bits);
+    //portfolioObjectList.push(bits);
+    portfolioObjectList[bits.id] = bits;
 
     // Puddle
     puddle = new PortfolioObject();
+    puddle.id = 'puddle';
     puddle .display = '../data/features/puddle/Puddle.png';
     puddle .title = 'Puddle';
     puddle .overview = 'Unity 3D project exploring 3D asset utilization from 3rd party sources';
@@ -33,10 +40,12 @@ function FillPortfolioSection(){
     puddle .tagSet.push('unity');
     puddle .featureSet.push('../data/features/bits-of-shadow/Switch.gif','../data/features/bits-of-shadow/TestScene.gif');
     puddle .other.push('Test', 'Object','Data');
-    portfolioObjectList.push(puddle );
+    //portfolioObjectList.push(puddle);
+    portfolioObjectList[puddle.id] = puddle;
 
     // Lost in Space
     space = new PortfolioObject();
+    space.id = 'space';
     space.display = '../data/features/lost-in-space/LostSpace.png';
     space.title = 'Lost in Space';
     space.overview = 'Unity 2D project exploring 2D game asset creation from concept to digitalization';
@@ -44,10 +53,13 @@ function FillPortfolioSection(){
     space.tagSet.push('unity');
     space.featureSet.push('../data/features/bits-of-shadow/Switch.gif','../data/features/bits-of-shadow/TestScene.gif');
     space.other.push('Test', 'Object','Data');
-    portfolioObjectList.push(space);
+    //portfolioObjectList.push(space);
+    portfolioObjectList[space.id] = space;
+
 
     // 3D Model
     model = new PortfolioObject();
+    model.id = 'model';
     model.display = '../data/features/modeling/Models.jpg';
     model.title = '3D Printing Weapons';
     model.overview = '3D models created in Autodesk Maya in preparation for later 3D printing';
@@ -55,10 +67,13 @@ function FillPortfolioSection(){
     model.tagSet.push('model');
     model.featureSet.push('../data/features/modeling/AxeModel.gif','../data/features/modeling/Models.gif');
     model.other.push('Test', 'Object','Data');
-    portfolioObjectList.push(model);
+    //portfolioObjectList.push(model);
+    portfolioObjectList[model.id] = model;
+
 
     // Python GAN
     gan = new PortfolioObject();
+    gan.id = 'gan';
     gan.display = '../data/features/gan/GAN Training.png';
     gan.title = 'Python - Machine Learning GAN';
     gan.overview = 'Python Generative Adversarial Network to generate images of handwritten digits';
@@ -66,29 +81,31 @@ function FillPortfolioSection(){
     gan.tagSet.push('python');
     gan.featureSet.push('../data/features/gan/GAN python.jpg','../data/features/gan/Class dist.jpg');
     gan.other.push('Test', 'Object','Data');
-    portfolioObjectList.push(gan);
+    //portfolioObjectList.push(gan);
+    portfolioObjectList[gan.id] = gan;
 }
 
-function BuildPortfolioObject(){
+// Create the cards and fill them
+function CreatePortfolioObjects(){
     
-    //use a portfolio object item here to fill a created card
+    // Use a portfolio object item here to fill a created card
     const displayArea = document.getElementById("display-area");
-    for (var i = 0; i < portfolioObjectList.length; i++){
-        
-        // ! Would likely be better to create these class items on another page instead of creating on fly like this?
+
+    Object.keys(portfolioObjectList).forEach(portItem => {
+        // ! Would likely be better to create these class items on another page instead of creating on fly like this? Hide them?
         var card = document.createElement('div');
-        card.classList.add('card');
-        card.classList.add('port');
+        card.id = portItem.id;
+        card.classList.add('portfolio-object');
         displayArea.appendChild(card);
 
-        card.innerHTML += `<h2>${portfolioObjectList[i].title}</h2><p>${portfolioObjectList[i].overview}</p>`;
+        card.innerHTML += `<h2>${portItem.title}</h2><p>${portItem.overview}</p>`;
         var demoImg = document.createElement('img');
         demoImg.classList.add('demo-image');
-        demoImg.src = portfolioObjectList[i].display;
+        demoImg.src = portItem.display;
         card.appendChild(demoImg);
 
         // Let's create a few tags for the object
-        portfolioObjectList[i].tagSet.forEach(element => {
+        portItem.tagSet.forEach(element => {
             var tmpTag = document.createElement('div');
             tmpTag.classList.add('demo-tag');
             tmpTag.innerHTML += element;
@@ -107,10 +124,51 @@ function BuildPortfolioObject(){
             }
             card.appendChild(tmpTag);
         });
-    }
+    });
+
+    portfolioObjectList.forEach(portItem => {
+
+    });
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
-    FillPortfolioSection();
-    BuildPortfolioObject();
+    FillPortfolioObjects();
+    CreatePortfolioObjects();
+    
+    function OpenModal(portObject){
+        // ----------------------------------------------------------------- TO DO:: modify list and function above to take dictionary instead
+        // This would be better in a dictionary for this next part to just built a new portfolioObject based on the id as the key
+        if(portObject.id in portfolioObjectList){
+            var portfolioObject = portfolioObjectList[portObject.id];
+            modal.innerHTML += `<h2>${portfolioObject.title}</h2><p>${portfolioObject.overview}</p>`;
+            modal.style.display = 'block';
+        }
+    }
+
+    function CloseModal(){
+        modal.style.display = 'none';
+    }
+
+    // Close the Modal initially
+    const modal = document.getElementById("modal");
+    CloseModal();
+    
+    // Grab all portfolio objects and add events for them to fill modal
+    const portObjects = document.querySelectorAll('.portfolio-object');
+    portObjects.forEach(portObject => {
+        portObject.addEventListener('click', () => OpenModal(portObject));
+    });
+
+    // Close when clicking outside of modal
+    window.addEventListener('click', event => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+    
+    // Close button functionality
+    const closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', closeModal);
+
 });
